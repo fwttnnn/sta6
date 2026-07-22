1750a142e1a1218d45bf69069ad0c5828df4b67a
fwttnnn <lolywk@tutanota.com>
2026-07-23 01:02:58 +0700

    refactor(sta6/build): `sta6:walk` isn't usable if there's dynamic route

--- sta6.lisp
+++ sta6.lisp
@@ -2,7 +2,6 @@
   (:use #:cl)
   (:export #:spit
            #:walk
-           #:render
            #:build
            #:html
            #:html5))
@@ -21,9 +20,6 @@
     (mapcan #'sta6:walk
             (uiop:subdirectories dir))))
 
-(defun sta6:render (args)
-  ())
-
 (defun sta6:build ()
   (let ((root (uiop:ensure-directory-pathname (truename "src/pages/"))))
     (flet ((extract-package-name (filepath)
@@ -33,36 +29,56 @@
                (format nil "~{~a/~}~a"
                            dirs
                            (pathname-name filepath))))
-           (dynamic-route-p (route)
-             (some (lambda (slug)
-                     (and (>= (length slug) 3)
-                           (char= (char slug 0) #\+)
-                           (char= (char slug (1- (length slug))) #\+)))
-                   (uiop:split-string route :separator "/"))))
-      (labels ((render (dir args)
-                 (dolist (filepath (uiop:directory-files dir))
-                   (let* ((pkg-name (extract-package-name filepath))
-                          (pkg (find-package (string-upcase (concatenate 'string "pages/" pkg-name))))
-                          (dirs (uiop:split-string pkg-name :separator "/"))
-                          (filename (car (last dirs)))
-                          (output-path (cond ((string= filename "404")  (make-pathname :directory '(:relative "build") :name "404" :type "html"))
-                                             ((string= filename "page") (make-pathname :directory `(:relative "build" ,@(butlast dirs)) :name "index" :type "html"))
-                                             (t                         (make-pathname :directory `(:relative "build" ,@(butlast dirs) ,filename) :name "index" :type "html"))))
-                          (symbol-routes (find-symbol "ROUTES" pkg))
-                          (symbol-render (find-symbol "RENDER" pkg)))
-                     (if symbol-routes
-                         (dolist (route (apply (symbol-function symbol-routes) args))
-                           (apply #'sta6:spit
-                                  (make-pathname :directory `(:relative "build" ,@(butlast dirs) ,(princ-to-string route)) :name "index" :type "html")
-                                  (symbol-function symbol-render)
-                                  (append args (list route))))
-                         (apply #'sta6:spit
-                                output-path
-                                (symbol-function symbol-render)
-                                args))))
-                 (dolist (child (uiop:subdirectories dir))
-                   (render child args))))
-        (render root '())))))
+           (replace-dynamic-route (slugs args)
+             (let ((args (copy-list args)))
+               (mapcar (lambda (slug)
+                         (if (and (>= (length slug) 3)
+                                  (char= (char slug 0) #\+)
+                                  (char= (char slug (1- (length slug))) #\+))
+                             (princ-to-string (pop args))
+                             slug))
+                       slugs))))
+      (labels ((recurse (dir args)
+                 (let ((saved-routes nil))
+                   (dolist (filepath (sort (copy-list (uiop:directory-files dir))
+                                           (lambda (a b)
+                                             (cond ((string= (pathname-name a) "page") t)
+                                                   ((string= (pathname-name b) "page") nil)
+                                                   (t nil)))))
+                     (let* ((pkg-name (extract-package-name filepath))
+                            (pkg (find-package (string-upcase (concatenate 'string "pages/" pkg-name))))
+                            (dirs (uiop:split-string pkg-name :separator "/"))
+                            (filename (car (last dirs)))
+                            (output-path (cond ((string= filename "404")  (make-pathname :directory '(:relative "build") :name "404" :type "html"))
+                                               ((string= filename "page") (make-pathname :directory `(:relative "build" ,@(butlast dirs)) :name "index" :type "html"))
+                                               (t                         (make-pathname :directory `(:relative "build" ,@(butlast dirs) ,filename) :name "index" :type "html"))))
+                            (symbol-routes (find-symbol "ROUTES" pkg))
+                            (symbol-render (find-symbol "RENDER" pkg)))
+                       ;; TODO: check if parent is a dynamic slug
+                       (if symbol-routes
+                           (progn
+                             (setf saved-routes (apply (symbol-function symbol-routes) args))
+                             (dolist (route saved-routes)
+                               (apply #'sta6:spit
+                                      (make-pathname :directory `(:relative "build" ,@(replace-dynamic-route (butlast dirs) (append args (list route)))) :name "index" :type "html")
+                                      (symbol-function symbol-render)
+                                      (append args (list route)))))
+                           (if saved-routes
+                             (dolist (route saved-routes)
+                               (apply #'sta6:spit
+                                      (make-pathname :directory `(:relative "build" ,@(replace-dynamic-route (butlast dirs) (append args (list route))) ,filename) :name "index" :type "html")
+                                      (symbol-function symbol-render)
+                                      (append args (list route))))
+                             (apply #'sta6:spit
+                                    output-path
+                                    (symbol-function symbol-render)
+                                    args)))))
+                   (dolist (sub-dir (uiop:subdirectories dir))
+                     (if saved-routes
+                         (dolist (route saved-routes)
+                           (recurse sub-dir (append args (list route))))
+                         (recurse sub-dir args))))))
+        (recurse root '())))))
 
 (defmacro sta6:html (&rest tags)
   `(spinneret:with-html
